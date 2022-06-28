22b254058fc39f8dd0aad483d0d4fe7031551c37
omi-72 <jannat.omi722@gmail.com>
2022-06-28 11:54:20 +0600

    Registration Complete AppcompatButton

--- app/src/main/java/com/example/chat/StartActivity.java
+++ app/src/main/java/com/example/chat/StartActivity.java
@@ -15,6 +15,8 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        btn_register = findViewById(R.id.btn_register);
+
         btn_register.setOnClickListener(view -> {
             startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
         });
