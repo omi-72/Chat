e10d7183efcec7f257832b52f90c9e0579633e64
omi-72 <jannat.omi722@gmail.com>
2022-06-28 12:32:55 +0600

    Register activity id findout & btn_register, btn haveanaccount clickable

--- app/src/main/java/com/example/chat/RegisterActivity.java
+++ app/src/main/java/com/example/chat/RegisterActivity.java
@@ -1,14 +1,36 @@
 package com.example.chat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class RegisterActivity extends AppCompatActivity {
+
+        TextInputEditText edt_username, edt_email, edt_password;
+        AppCompatButton btn_register, btn_have_an_account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        edt_username = findViewById(R.id.edt_username);
+        edt_email = findViewById(R.id.edt_email);
+        edt_password = findViewById(R.id.edt_password);
+        btn_register = findViewById(R.id.btn_register);
+        btn_have_an_account = findViewById(R.id.btn_have_an_account);
+
+        btn_register.setOnClickListener(v -> {
+
+        });
+
+        btn_have_an_account.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(),LogInActivity.class));
+            finish();
+        });
     }
 }
