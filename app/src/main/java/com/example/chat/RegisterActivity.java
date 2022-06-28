b3fc10415228049349ce6a5f76a32167241a9695
omi-72 <jannat.omi722@gmail.com>
2022-06-28 19:07:02 +0600

    Hashmap with usermap

--- app/src/main/java/com/example/chat/RegisterActivity.java
+++ app/src/main/java/com/example/chat/RegisterActivity.java
@@ -7,16 +7,24 @@ import androidx.appcompat.widget.AppCompatButton;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
         TextInputEditText edt_username, edt_email, edt_password;
         AppCompatButton btn_register, btn_have_an_account;
+
+        FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +42,7 @@ public class RegisterActivity extends AppCompatActivity {
             String password= edt_password.getText().toString().trim();
             String username= edt_username.getText().toString().trim();
 
-            registerUser(email, password);
+            registerUser(email, password, username);
 
         });
 
@@ -44,12 +52,32 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    private void registerUser(String email, String password) {
+    private void registerUser(String email, String password, String username) {
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
         firebaseAuth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
             @Override
             public void onSuccess(AuthResult authResult) {
+                firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+                String userId= firebaseUser.getUid();
+
+                HashMap<String, Object> userMap= new HashMap<>();
+
+                userMap.put("Name", username);
+                userMap.put("email", email);
+                userMap.put("profileImage", "");
+                userMap.put("coverImage", "");
+                userMap.put("bio", "");
+
+                FirebaseDatabase.getInstance().getReference("user").child(userId)
+                                .setValue(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+
+                            }
+                        })
+
+
                 startActivity(new Intent(getApplicationContext(),MainActivity.class));
                 finish();
 
