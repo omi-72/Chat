a706f9c2261d929ea9091805c85180c71d023b4f
omi-72 <jannat.omi722@gmail.com>
2022-06-28 11:05:25 +0600

    Register & Sign In Button Clickable & also new Register activity pass intent

--- app/src/main/java/com/example/chat/StartActivity.java
+++ app/src/main/java/com/example/chat/StartActivity.java
@@ -1,14 +1,23 @@
 package com.example.chat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class StartActivity extends AppCompatActivity {
+
+    AppCompatButton btn_signIn, btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        btn_register.setOnClickListener(view ->{
+        startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+
+        });
     }
 }
