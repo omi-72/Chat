645bd0aa3ee129e8d57912cfbbe5462b3d5253e5
omi-72 <jannat.omi722@gmail.com>
2022-06-28 13:20:05 +0600

    String & registerUser email, password method successfully or failor Listener & succesfully intent On mainActivity

--- app/src/main/java/com/example/chat/RegisterActivity.java
+++ app/src/main/java/com/example/chat/RegisterActivity.java
@@ -1,12 +1,17 @@
 package com.example.chat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -25,12 +30,35 @@ public class RegisterActivity extends AppCompatActivity {
         btn_have_an_account = findViewById(R.id.btn_have_an_account);
 
         btn_register.setOnClickListener(v -> {
+            String email= edt_email.getText().toString().trim();
+            String password= edt_password.getText().toString().trim();
+            String username= edt_username.getText().toString().trim();
+
+            registerUser(email, password);
 
         });
 
         btn_have_an_account.setOnClickListener(v -> {
             startActivity(new Intent(getApplicationContext(),LogInActivity.class));
             finish();
+        });
+    }
+
+    private void registerUser(String email, String password) {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
         });
     }
 }
