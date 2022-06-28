9b9e5c598378b6d131795c6c56a3d441b44f5ddd
omi-72 <jannat.omi722@gmail.com>
2022-06-28 18:26:30 +0600

    firbaseAuth user findAccount & Logout

--- app/src/main/java/com/example/chat/StartActivity.java
+++ app/src/main/java/com/example/chat/StartActivity.java
@@ -6,14 +6,20 @@ import androidx.appcompat.widget.AppCompatButton;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class StartActivity extends AppCompatActivity {
 
     AppCompatButton btn_register, btn_logIn;
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
 
         btn_register = findViewById(R.id.btn_register);
         btn_logIn = findViewById(R.id.btn_logIn);
@@ -25,6 +31,16 @@ public class StartActivity extends AppCompatActivity {
         btn_logIn.setOnClickListener(view -> {
             startActivity(new Intent(getApplicationContext(),LogInActivity.class));
         });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (firebaseUser != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
 
     }
 }
