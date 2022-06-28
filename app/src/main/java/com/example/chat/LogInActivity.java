b12a299488aeb54cc96d04cb99a39fdc9467c8e6
omi-72 <jannat.omi722@gmail.com>
2022-06-28 13:34:52 +0600

    add logIn listener with email & password

--- app/src/main/java/com/example/chat/LogInActivity.java
+++ app/src/main/java/com/example/chat/LogInActivity.java
@@ -1,12 +1,17 @@
 package com.example.chat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogInActivity extends AppCompatActivity {
     TextInputEditText edt_email, edt_password;
@@ -23,6 +28,10 @@ public class LogInActivity extends AppCompatActivity {
         btn_create_an_account = findViewById(R.id.btn_create_an_account);
 
         btn_logIn.setOnClickListener(v -> {
+            String email= edt_email.getText().toString().trim();
+            String password= edt_password.getText().toString().trim();
+
+           loginUser(email, password);
 
         });
 
@@ -31,5 +40,24 @@ public class LogInActivity extends AppCompatActivity {
             finish();
         });
 
+    }
+
+    private void loginUser(String email, String password) {
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
     }
 }
