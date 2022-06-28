029509176017af1bbc0fcbbb51a899cf1b365e54
omi-72 <jannat.omi722@gmail.com>
2022-06-28 19:48:56 +0600

    find id viewPager & tablayout

--- app/src/main/java/com/example/chat/MainActivity.java
+++ app/src/main/java/com/example/chat/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.chat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager= findViewById(R.id.fragmentViewPager);
+        tabLayout=findViewById(R.id.tabLayout);
     }
 
     public void SignOut(View view) {
