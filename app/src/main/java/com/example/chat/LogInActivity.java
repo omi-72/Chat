3b9d6f16d94708bbf1575939333adf9d11348663
omi-72 <jannat.omi722@gmail.com>
2022-06-28 13:01:05 +0600

    Log in xml id find & clickable

--- app/src/main/java/com/example/chat/LogInActivity.java
+++ app/src/main/java/com/example/chat/LogInActivity.java
@@ -1,14 +1,35 @@
 package com.example.chat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class LogInActivity extends AppCompatActivity {
+    TextInputEditText edt_email, edt_password;
+    AppCompatButton btn_logIn, btn_create_an_account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        edt_email = findViewById(R.id.edt_email);
+        edt_password = findViewById(R.id.edt_password);
+        btn_logIn = findViewById(R.id.btn_logIn);
+        btn_create_an_account = findViewById(R.id.btn_create_an_account);
+
+        btn_logIn.setOnClickListener(v -> {
+
+        });
+
+        btn_create_an_account.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
+            finish();
+        });
+
     }
 }
