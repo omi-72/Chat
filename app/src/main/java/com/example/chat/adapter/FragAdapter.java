51b6b9ba9a3972d0d427d2c0264dd2d24c999c64
omi-72 <jannat.omi722@gmail.com>
2022-06-30 11:57:44 +0600

    for limit decler as String Status, User, Chate & return names.length & also stich on case 0,1,2 as Status,user,chat fragment

--- app/src/main/java/com/example/chat/adapter/FragAdapter.java
+++ app/src/main/java/com/example/chat/adapter/FragAdapter.java
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.example.chat.fragments.ChatFragment;
+import com.example.chat.fragments.StatusFragment;
+import com.example.chat.fragments.UserFragment;
+
 public class FragAdapter extends FragmentPagerAdapter {
+
+    String[] names = {"Status", "User", "Chat"};
 
 
      public FragAdapter(@NonNull FragmentManager fm) {
@@ -15,11 +21,23 @@ public class FragAdapter extends FragmentPagerAdapter {
     @NonNull
     @Override
     public Fragment getItem(int position) {
+
+         switch (position){
+             case 0:
+                 return new StatusFragment();
+
+             case 1:
+                 return new UserFragment();
+
+             case 2:
+                 return new ChatFragment();
+         }
+
          return null;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return names.length;
     }
 }
