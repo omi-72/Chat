db558e35fa43c5567da56e125e3b3b702990793f
omi-72 <jannat.omi722@gmail.com>
2022-06-30 11:44:40 +0600

    Add package name by Adapter & on this Adapter create Java class name as FragAdapter

--- app/src/main/java/com/example/chat/fragments/ChatFragment.java
+++ app/src/main/java/com/example/chat/fragments/ChatFragment.java
@@ -19,7 +19,7 @@ public class ChatFragment extends Fragment {
         // Required empty public constructor
     }
 
-    
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
