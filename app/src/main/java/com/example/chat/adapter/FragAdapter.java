87a9e01ac1a791025d071472f59af87ac97cb5b7
omi-72 <jannat.omi722@gmail.com>
2022-06-30 11:49:47 +0600

    fragmentAdapter extend FragmentPagerAdapoter also create method & constructor on superClass

--- app/src/main/java/com/example/chat/adapter/FragAdapter.java
+++ app/src/main/java/com/example/chat/adapter/FragAdapter.java
@@ -1,4 +1,25 @@
 package com.example.chat.adapter;
 
-public class FragAdapter {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class FragAdapter extends FragmentPagerAdapter {
+
+
+     public FragAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+         return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
 }
